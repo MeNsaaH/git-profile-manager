d99424a403b184544f2a866c5007dee6f68f4720
Manasseh Mmadu <mmadumanasseh@gmail.com>
2020-06-03 00:28:07 +0100

    Fixed installation

--- git_profile_manager/commands/global_config.py
+++ git_profile_manager/commands/global_config.py
@@ -7,7 +7,7 @@ from git_profile_manager.common_args import parent_parser
 def global_config(args):
     os.environ["GIT_CONFIG"] = utils.GLOBAL_GITCONFIG
 
-    utils.exec_command(["git", "config", args.key, args.config])
+    utils.exec_command(["git", "config", "--global", args.key, args.config])
     # After updating config, update the current users configuration aswell
     current_user = utils.get_current_user()
     utils.save_current_user_profile()
