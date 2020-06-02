74f91a8dd04321b1cdcf234e1dd0c6062ce5de70
Manasseh Mmadu <mmadumanasseh@gmail.com>
2020-06-02 12:02:38 +0100

    Update installation and commands to not require sourcing after every command

--- functions.sh
+++ functions.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 GIT_PROFILE_DIR="$HOME/.gitprofiles"
+# Store details of the current profile
+PROFILE_RC="$GIT_PROFILE_DIR/.profilerc"
 # zshrc or bashrc or the likes
 shell_config=
 username=
@@ -9,24 +11,44 @@ name=
 # Used to search for where to replace current config
 export_prefix="export GIT_CONFIG"
 
-get_user_shell_profile_config() {
+# LoadUP appropriate variables
+setup_env() {
+  # Load shell config file
   if [ "$SHELL" = "/usr/bin/zsh" ];then 
     shell_config="$HOME/.zshrc"
   elif [ "$SHELL" = "/usr/bin/bash" ]; then
     shell_config="$HOME/.bashrc"
   fi
-}
 
-
-update_profile() {
-  if grep -Fq "$export_prefix" $shell_config
-  then
-    sed -i "s|.*export GIT_CONFIG.*|$export_prefix=$GIT_PROFILE_DIR/$username|" "$shell_config"
-  else
-    echo "Git Runas config"
-    echo "$export_prefix=$GIT_PROFILE_DIR/$username" >> $shell_config
+  # Check if git-profile-manager is properly installed
+  if [ ! -d $GIT_PROFILE_DIR ]; then
+    printf "Git profile manager is not properly installed. Visit https://github.com/mensaah/git-profile-manager for how to install"
+    exit
   fi
 }
 
-get_user_shell_profile_config
+# Set the git config to new user's config
+update_profile() {
+  local username=$1
+  if grep -Fq "CURRENT_PROFILE" $PROFILE_RC
+  then
+    sed -i "s|.*CURRENT_PROFILE.*|CURRENT_PROFILE=$username|" "$PROFILE_RC"
+  else
+    printf "CURRENT_PROFILE=$username" >> $PROFILE_RC
+  fi
+  # Copy global profile to git config location and append user config at the bottom
+  cp "$GIT_PROFILE_DIR/global" "$HOME/.gitconfig"
+  cat $GIT_PROFILE_DIR/$username >> "$HOME/.gitconfig"
+}
+
+get_current_profile(){
+  # Maybe use source and load the variables directly
+  #  source $PROFILE_RC
+  current_profile=$(grep  'CURRENT_PROFILE' $PROFILE_RC)
+  IFS='='
+  read -ra ADDR <<< "$current_profile"
+  username="${ADDR[-1]}"
+}
+
+setup_env
 
