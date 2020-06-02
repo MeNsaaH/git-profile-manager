7d675395cd8804ec5e77c3cda8aae2838c76e1aa
Manasseh Mmadu <mmadumanasseh@gmail.com>
2020-06-02 20:36:47 +0100

    Removed bash scripts

--- profile-manager.sh
+++
@@ -1,6 +0,0 @@
-# profile-manager.sh
-#
-# Set's all required path variables required to run git-profile-manager
-
-export GIT_PROFILE_BIN_DIR="$HOME/.gitprofiles/bin"  
-export PATH=$PATH:$GIT_PROFILE_BIN_DIR
